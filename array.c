7d2566646515338029cea21e63f5fe3bd0c3322c
aruntejacse <53857149+aruntejacse@users.noreply.github.com>
2020-02-06 13:19:27 +0530

    Add files via upload

--- array.c
+++ array.c
@@ -3,7 +3,7 @@
 
 void search(int arr[], int key , int n)
 	{
-	int flag,count=0,first=0,last=n-1;
+	int flag,count=0,first=0,last=n-1,f_o,l_o;
 	int middle=(first+last)/2;
 	while(first<=last)
 		{
@@ -23,11 +23,73 @@ void search(int arr[], int key , int n)
 			{
 			if(arr[middle]==key && arr[middle+1]>key)
 				{
+				f_o=middle;
+				while(arr[middle]!=key)
+				{
 				count++;
 				first=0;
 				last=middle;
 				middle=(first+last)/2;
-				
+				}
+				l_o=middle;
+				}
+			else if(arr[middle]==key && arr[middle+1]>key)
+				{
+				l_o=middle;
+				while(arr[middle]!=key)
+				{
+				count++;
+				first=middle;
+				last=n-1;
+				middle=(first+last)/2;
+				}
+				f_o=middle;
+				}
+			else
+				{
+				while(arr[middle]!=key)
+				{
+				count++;
+				first=0;
+				last=middle;
+				middle=(first+last)/2;
+				}
+				f_o=middle;
+				while(arr[middle]!=key)
+				{
+				count++;
+				first=middle;
+				last=n-1;
+				middle=(first+last)/2;
+				}
+				l_o=middle;
+				}
+			}
+		}
+		
+void main()
+	{
+	int arr[100],n,key;
+	printf("Enter the number of elements:");
+	scanf("%d",&n);
+	printf("Enter the elements:");
+	for(int i=0;i<n;i++)
+		{
+		scanf("%d",&arr[i]);
+		}
+	printf("Enter the key:");
+	scanf("%d",&key);
+	search(arr[],key,n);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 					 
 			
 		
