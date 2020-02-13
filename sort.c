34644f47e667fb6018a0132f6be3ab64053314f6
aruntejacse <53857149+aruntejacse@users.noreply.github.com>
2020-02-13 12:00:41 +0530

    Add files via upload

---
+++ sort.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+void bubblesort(int arr[],int n)	
+	{
+	int temp;
+	for(int i =0;i<n;i++)
+		{
+		for(int j = i;j<n;j++)
+			{
+			if(arr[i]>arr[j])
+				{
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+				}
+			}
+		}
+	for(int i=0;i<n;i++)
+		{
+		printf("%d \n",arr[i]);
+		}
+	}
+
+void main()
+	{
+	clock_t start,end,ran;
+	double total;
+	int arr[100],n;
+	start = clock();
+	printf("Enter the number of elements:");
+	scanf("%d",&n);
+	printf("Entering the elements...\n");
+	for(int i =0;i<n;i++)
+		{
+		ran = rand()%100;
+		arr[i]=ran;
+		}
+	bubblesort(arr,n);
+	end=clock();
+	total=(double)(start-end)/CLOCKS_PER_SEC;
+	printf("%f",total);
+	}
+	
+	
